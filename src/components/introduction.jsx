32121fe9a6f1e634ee8e865acd92afaf4b793c66
Autumn Gehring <autumngehring@Autumns-MacBook-Pro.local>
2021-02-13 17:32:57 -0700

    update background

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,14 +7,14 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li className="bgimg" style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Autumn</h1>
+                          <h1 className="name">Hi! <br />I'm Autumn</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1jLEfnZjBezm6HApXUdyxckklPjzs8YNh/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
@@ -29,7 +29,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I Love Coding</h1>
+                          <h1 className="coding">I Love Coding</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://github.com/akichan89" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
