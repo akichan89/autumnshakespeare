252bb6cd38a6e7e81a285d8f09969913342c4b05
akichan89 <growingupcollegestyle@gmail.com>
2022-07-21 20:38:09 -0600

    changes to images

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,70 +1,136 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
+          <div className="colorlib-narrow-content">
             <div className="row">
-            <div className="col-md-12">
-                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
+              <div className="col-md-12">
+                <div
+                  className="row row-bottom-padded-sm animate-box"
+                  data-animate-effect="fadeInLeft"
+                >
+                  <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
-                    <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>Let me tell you a little bit about myself: I am a hard worker, determined, and love to live a positive life. I am Griffin Hill trained as well as CompTIA A+ certified which has made me very successful at making long time customers and money for companies. What this means for you and your company is: I will be a productive employee that will bring in revenue while making the customers feel comfortable and like I care about their needs, all while exceeding their expectations. Therefore, your company will have lifelong customers that will recommend you to their friends and family.</p>
-                    <p>I am also a very fun, upbeat, perky young woman that can add to the already happy environment you have. As if that weren’t enough, I can put a smile on anyone's face! I would be an asset to your company!!</p>
+                      <span className="heading-meta">About Me</span>
+                      <h2 className="colorlib-heading">Who Am I?</h2>
+                      <p>
+                        Let me tell you a little bit about myself: I am a hard
+                        worker, determined, and love to live a positive life. I
+                        am Griffin Hill trained as well as CompTIA A+ certified
+                        which has made me very successful at making long time
+                        customers and money for companies. What this means for
+                        you and your company is: I will be a productive employee
+                        that will bring in revenue while making the customers
+                        feel comfortable and like I care about their needs, all
+                        while exceeding their expectations. Therefore, your
+                        company will have lifelong customers that will recommend
+                        you to their friends and family.
+                      </p>
+                      <p>
+                        I am also a very fun, upbeat, perky young woman that can
+                        add to the already happy environment you have. As if
+                        that weren’t enough, I can put a smile on anyone's face!
+                        I would be an asset to your company!!
+                      </p>
                     </div>
+                  </div>
                 </div>
-                </div>
+              </div>
             </div>
-            </div>
-        </div>
+          </div>
         </section>
         <section className="colorlib-about">
-        <div className="colorlib-narrow-content">
+          <div className="colorlib-narrow-content">
             <div className="row">
-            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
                 <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
-            </div>
+                <h2 className="colorlib-heading">
+                  Here are some of my expertise
+                </h2>
+              </div>
             </div>
             <div className="row row-pt-md">
-            <div className="col-md-4 text-center animate-box">
+              <div className="col-md-4 text-center animate-box">
                 <div className="services color-1">
-                <span className="icon">
+                  <span className="icon">
                     <i className="icon-bulb" />
-                </span>
-                <div className="desc">
+                  </span>
+                  <div className="desc">
                     <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <p>
+                      I have experience building websites and chrome extentions
+                      using JavaScript,React,HTML,CSS. I work with Python and
+                      Flask for backend development and can pair that experience
+                      with PSQL, MongoDB and GraphQL.
+                    </p>
+                    <br></br>
+
+                    <a
+                      href="https://ladyndev.blogspot.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Read More
+                    </a>
+                  </div>
                 </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
+              </div>
+              <div className="col-md-4 text-center animate-box">
                 <div className="services color-3">
-                <span className="icon">
+                  <span className="icon">
                     <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                  </span>
+                  <div className="desc">
+                    <h3>IT Professional</h3>
+                    <p>
+                      I am CompTIA A+ certified and can do everything from basic
+                      hardware support to networking, working with AD, various
+                      MDM software, AD, MacOS, Windows and Linux. As a bonus, I
+                      can cable my own ethernet cables!
+                    </p>
+                    <br></br>
+
+                    <a
+                      href="https://ladyndev.blogspot.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Read More
+                    </a>
+                  </div>
                 </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
+              </div>
+              <div className="col-md-4 text-center animate-box">
                 <div className="services color-5">
-                <span className="icon">
+                  <span className="icon">
                     <i className="icon-data" />
-                </span>
-                <div className="desc">
+                  </span>
+                  <div className="desc">
                     <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <p>
+                      I have experience mangaing hosting and deployemnt through
+                      DigitalOcean and establishiing a CI/CD pipeline with
+                      GitHub Actions. I have understanding and training working
+                      with AWS and Google Cloud Platform as well. For fun, I
+                      learned Jenkins, Chef and Ansible.
+                    </p>
+                    <a
+                      href="https://ladyndev.blogspot.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Read More
+                    </a>
+                  </div>
                 </div>
-                </div>
-            </div>
-            {/*
+              </div>
+              {/*
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-2">
                 <span className="icon">
@@ -100,9 +166,9 @@ export default class About extends Component {
             </div>
             */}
             </div>
-        </div>
+          </div>
         </section>
       </div>
-    )
+    );
   }
 }
--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,51 +1,135 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import pi from "../photos/pi.JPG";
+import me from "../photos/learning.JPG";
+import image from "../photos/image.JPG";
 
 export default class Introduction extends Component {
   render() {
     return (
       <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+        <section
+          id="colorlib-hero"
+          className="js-fullheight"
+          data-section="home"
+        >
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li
+                style={{
+                  backgroundImage: `url(${image})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "contain",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  height: 700,
+                  width: "100vw",
+                  opacity: 0.2,
+                }}
+              >
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Autumn</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1jLEfnZjBezm6HApXUdyxckklPjzs8YNh/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <h1 style={{ color: "#FF4800" }}>
+                            Hi! <br />
+                            I'm Autumn Shakespeare
+                          </h1>
+                          <p>
+                            <a
+                              style={{ color: "#8950C9" }}
+                              className="btn btn-primary btn-learn"
+                              href="https://drive.google.com/file/d/1pUFJUjGAkGUzzypS5Bg3UyKAQ2rL-U9X/view?usp=sharing"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Resume
+                              <i className="icon-download4" />
+                            </a>
+                          </p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li
+                style={{
+                  backgroundImage: `url(${me})`,
+                  backgroundRepeat: "no-repeat",
+                  // backgroundSize: "contain",
+
+                  // backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  // backgroundSize: "cover",
+                  height: 700,
+                  width: "100vw",
+                  opacity: 0.2,
+                }}
+              >
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <h1 style={{ color: "#FF4800" }}>
+                            I love learning new tech and building
+                            <br /> THINGS!!
+                          </h1>
+                          <p>
+                            <a
+                              style={{ color: "#8950C9" }}
+                              className="btn btn-primary btn-learn"
+                              href="https://github.com/akichan89"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Projects <i className="icon-briefcase3" />
+                            </a>
+                          </p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li
+                style={{
+                  backgroundImage: `url(${pi})`,
+                  backgroundRepeat: "no-repeat",
+                  // backgroundSize: "contain",
+
+                  // backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  // backgroundSize: "cover",
+                  height: 700,
+                  width: "100vw",
+                  opacity: 0.2,
+                }}
+              >
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          <h1 style={{ color: "#FF4800" }}>
+                            I often <br />
+                            play with Arduinos and PIs...{" "}
+                          </h1>
+                          <p>
+                            <a
+                              style={{ color: "#8950C9" }}
+                              className="btn btn-primary btn-learn"
+                              href="https://ladyndev.blogspot.com/"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Blog <i className="icon-book" />
+                            </a>
+                          </p>
                         </div>
                       </div>
                     </div>
@@ -56,6 +140,6 @@ export default class Introduction extends Component {
           </div>
         </section>
       </div>
-    )
+    );
   }
 }
